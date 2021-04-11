037d40e38f5e899e68d17b2e30f8e958b1f023bc
sergeykotenko <sergeykotenko@bk.ru>
2021-04-11 15:07:07 +0300

    finish adaptive

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let a = 'Hello'
+const EngRus = document.querySelector('.change-country')
+// const EngRus = document.querySelector('.show-country')
+const changeCountry = document.querySelector('.country')
+changeCountry.addEventListener('click', (e) => {
+    // console.log("Привет")
+    // EngRus.
+    EngRus.classList.toggle('show-country');
+})
